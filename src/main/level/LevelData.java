098bb9595dd26b741ca7ca68998585cd4f6b80dc
pwnzor1337 <kaleb.mcclenathan@gmail.com>
2017-06-13 11:30:09 -0500

    Added TODO

--- src/main/level/LevelData.java
+++ src/main/level/LevelData.java
@@ -1,5 +1,11 @@
 package main.level;
 
+//TODO: change level data to be more readable
+//TODO: red   : enemy entity
+//TODO: green : ally entity
+//TODO: black : wall/colision tile
+//TODO: white : open space/no colision tile
+//TODO: blue  : items
 public enum LevelData {
 	full, empty, player, zombie;
 }
